fc84bf2beaa1f7f35fa6ce92ec0a9928eb4f1e6b
Renata Menezes <86611407+RenataDayana@users.noreply.github.com>
2023-01-02 06:09:04 -0300

    exercicio 4 - Descobrindo o tipo primitivo de um valor

---
+++ ex004.py
@@ -0,0 +1,6 @@
+v=input('Digite algo:')
+print('O tipo primitivo desse valor é', type(v))
+print('Só tem espaços?',v.isspace())
+print('É um número?',v.isnumeric())
+print('É alfabético?',v.isalpha())
+
